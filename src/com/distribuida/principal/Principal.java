7424be0d12e7f4c3b1c65bc4c3de2c55456cc914
ShadenJels <andresh@tecnologicoismac.edu.ec>
2023-11-17 09:53:15 -0500

    " V0002 seguno cambio"

--- src/com/distribuida/principal/Principal.java
+++ src/com/distribuida/principal/Principal.java
@@ -1,13 +1,15 @@
 package com.distribuida.principal;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("El DIABLO LOCO......");
+		System.out.println("El DIABLO LOCO......");
+		System.out.println("El DIABLO LOCO......");
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 	}
 
